56da08e1d0397ca196915817619ce34838a0a555
SamarthAnnigeri <Samarthgrand@gmail.com>
2022-09-17 21:56:43 -0700

    Initial Commit

---
+++ Movie Image with Calc/Movie_Image_with_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Image_with_CalcApp.swift
+//  Movie Image with Calc
+//
+//  Created by Samarth Annigeri on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Image_with_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
